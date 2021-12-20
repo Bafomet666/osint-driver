4483b6e6f2d787382537e9e730321df383a9d140
Максим <72593411+Bafomet666@users.noreply.github.com>
2021-12-20 19:47:19 +0000

    Update driver.py

--- driver/driver.py
+++ driver/driver.py
@@ -4,32 +4,39 @@ import os
 
 page_1 = fr'''
 
- _____     ______     __     __   __   ______     ______    
-/\  __-.  /\  == \   /\ \   /\ \ / /  /\  ___\   /\  == \   
-\ \ \/\ \ \ \  __<   \ \ \  \ \ \'/   \ \  __\   \ \  __<   
- \ \____-  \ \_\ \_\  \ \_\  \ \__|    \ \_____\  \ \_\ \_\ 
-  \/____/   \/_/ /_/   \/_/   \/_/      \/_____/   \/_/ /_/ 
+
+  _____     ______     __     __   __   ______     ______    
+ /\  __-.  /\  == \   /\ \   /\ \ / /  /\  ___\   /\  == \   
+ \ \ \/\ \ \ \  __<   \ \ \  \ \ \'/   \ \  __\   \ \  __<   
+  \ \____-  \ \_\ \_\  \ \_\  \ \__|    \ \_____\  \ \_\ \_\ 
+   \/____/   \/_/ /_/   \/_/   \/_/      \/_____/   \/_/ /_/ 
                                                             
 
-
+ Установка pycurl, geckodriver + selenium, vlc
+ 
+ driver osintsan pro 5+
+ 
+ [ version 1.0 ]
 '''
 
 
 def driver():
     print(' Установка pycurl')
-    os.system('sudo apt install libcurl4-gnutls-dev librtmp-dev sudo apt-get install libcurl4-gnutls-dev librtmp-dev pip3 install setuptools pip3 install pycurl pip3 install grab')
-    os.system('sudo su')
+    os.system('sudo apt install libcurl4-gnutls-dev librtmp-dev')
+    os.system('sudo apt-get install libcurl4-gnutls-dev librtmp-dev')
+    os.system('pip3 install setuptools')
+    os.system('pip3 install pycurl')
+    os.system('pip3 install grab')
+   
     print(' Установка webdriver')
     os.system('sudo chmod +x /usr/local/bin/geckodriver')
     os.system('sudo mv geckodriver /usr/local/bin/geckodriver')
     os.system('sudo chown root:root /usr/local/bin/geckodriver')
-    os.system('wget https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar')
-    os.system('sudo java -jar selenium-server-standalone-3.141.59.jar')
+    
     print(' Установка vlc')
     os.system('pip3 install python-vlc')
     os.system('sudo apt-get install vlc')
-    
-    
-driver()
+    print(' \nУстановка завершена')
+    os.system('exit')
     
     
